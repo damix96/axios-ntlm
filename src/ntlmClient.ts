1634de033e5559114035b868e2fec8eff68c4260
Damir <damir.ashmakhunov@eubank.kz>
2022-10-07 05:02:26 +0600

    log req headers

--- src/ntlmClient.ts
+++ src/ntlmClient.ts
@@ -53,7 +53,8 @@ export function NtlmClient(
 		async (err: AxiosError) => {
 			const error: AxiosResponse | undefined = err.response;
 			const wwwAuthenticateHeader: string = error?.headers["www-authenticate"];
-
+			console.log("request headers", error?.request?.headers);
+			console.log(error?.request?.toJSON());
 			if (error?.status === 401 && wwwAuthenticateHeader.includes("NTLM")) {
 				// This length check is a hack because SharePoint is awkward and will
 				// include the Negotiate option when responding with the T2 message
@@ -83,7 +84,7 @@ export function NtlmClient(
 					);
 					console.log("T3 message created", t3Msg);
 
-					error.config.headers["X-retry"] = "false";
+					// error.config.headers["X-retry"] = "false";
 					error.config.headers["Authorization"] = t3Msg;
 				}
 
